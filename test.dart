0a8e0b4465b4273daa0796798dc38a467fc62b7a
kokofin <muhammadnoorarifin94@gmail.com>
2023-02-02 14:16:04 +0800

    variable pertama

---
+++ test.dart
@@ -0,0 +1,12 @@
+void main() {
+  //deklarasi
+  int age ;
+  String name ;
+  //value
+  name = 'Arifin';
+  age = 28;
+  //output
+  print('Nama Saya Adalah $name , Dan Umur Saya Adalah $age');
+  print('Nama Saya Adalah $name , Dan Umur Saya 10 Tahun Kedepan Adalah , ${age+10} Tahun');
+  print ('Nama Saya Adalah ,'  +  name +  ' Dan Umur Saya Adalah ,' + age.toString() + 'Tahun ,');
+}
